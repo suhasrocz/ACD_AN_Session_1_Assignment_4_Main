5c1bf10422f83b822090f822940a8c967282ff3f
suhasrocz <suhasrocz@gmail.com>
2016-09-16 12:30:46 +0530

    ACD_AN_Session_1_Assignment_4_Main
    Hello World displayed in android device

---
+++ HelloWorld/app/src/main/java/com/helloworld/suhas/helloworld/MainActivity.java
@@ -0,0 +1,39 @@
+package com.helloworld.suhas.helloworld;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
